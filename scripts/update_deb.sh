e1b2422b498277266e8272a9f364011bbd3514f2
LEE <15267030696@163.com>
2023-04-26 11:00:58 +0800

    1.新增脚本；

---
+++ scripts/update_deb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 路径
+BASEDIR=$(dirname "$0")
+cd ${BASEDIR}
+BASEDIR=`pwd`
+# 根目录
+ROOT_DIR=${BASEDIR}/../
+source env.sh
+
+
+cp ${changerootfs_exec} ${changerootfs_exec_dist}
+
+cp ${kernbypassd_exec} ${kernbypassd_exec_dist}
+
+cp ${preparerootfs_exec} ${preparerootfs_exec_dist}
+
+cp ${prerm_exec} ${prerm_exec_dist}
+
+cp ${KernBypassdCC_bundle} ${KernBypassdCC_bundle_dist}
+
+cp ${KernBypassPrefs_bundle} ${KernBypassPrefs_bundle_dist}
+
+cp ${zzzzzzzzzNotifyChroot_dylib} ${zzzzzzzzzNotifyChroot_dylib_dist}
+
+
+
+
+
+
